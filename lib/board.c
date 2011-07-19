6ff12b34da6f2664ed3d1d534327910993c29d58
Manel Caro <mcaro@iseebcn.com>
2011-07-19 14:29:20 +0200

    Update Version string

--- lib/board.c
+++ lib/board.c
@@ -42,7 +42,7 @@
 #endif
 
 const char version_string[] =
-	"IGEP-X-Loader 2.1.0-1 (" __DATE__ " - " __TIME__ ")";
+	"IGEP-X-Loader 2.1.0-2 (" __DATE__ " - " __TIME__ ")";
 
 int print_info(void)
 {
